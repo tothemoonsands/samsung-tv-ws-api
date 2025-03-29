ac929b1dd74376948513d71e64bbb83bf99f528e
Nick Waterton <nick.waterton@med.ge.com>
2025-03-29 12:50:48 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     url="https://github.com/NickWaterton/samsung-tv-ws-api",
     package_data={"samsungtvws": ["py.typed"]},
     packages=find_packages(exclude=("tests",)),
-    install_requires=["websocket-client>=0.57.0", "requests>=2.21.0", "aiohttp>=3.8.1", "websockets>=10.2"],
+    install_requires=["websocket-client>=0.57.0", "requests>=2.21.0", "aiohttp>=3.8.1", "websockets>=10.2", "async_timeout>=4.0.3"],
     extras_require={
         "encrypted": ["cryptography>=35.0.0", "py3rijndael>=0.3.3"],
     },
